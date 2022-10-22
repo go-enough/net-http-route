834914e0379bcd32cbdd496b85372529fd7cde06
M1xA <AnyCPU@users.noreply.github.com>
2022-10-23 00:44:41 +0300

    doc: better example with graceful shutdown

--- pkg/route/route.go
+++ pkg/route/route.go
@@ -335,13 +335,29 @@ func EnsureMethods(w http.ResponseWriter,
 //	package main
 //
 //	import (
+//		"context"
 //		"log"
+//		"net"
 //		"net/http"
+//		"os/signal"
+//		"syscall"
+//		"time"
 //
 //		"github.com/go-enough/net-http-route/pkg/route"
 //	)
 //
+//	const addr = ":8080"              // default address and port: 0.0.0.0:8080
+//	const ttl = 10                    // seconds
+//	const terminate = syscall.SIGTERM // OS says to terminate
+//
 //	func main() {
+//		log.Printf("source=%q msg=%q\n", "main", "hi")
+//		defer log.Printf("source=%q msg=%q\n", "main", "bye")
+//
+//		// Listening to OS signals.
+//		ctx, shutdown := signal.NotifyContext(context.Background(), terminate)
+//		defer shutdown()
+//
 //		// Creating a new route object and setting a default HTTP handler.
 //		app := route.New(http.NotFoundHandler())
 //
@@ -357,7 +373,7 @@ func EnsureMethods(w http.ResponseWriter,
 //		// or
 //		// `curl -v "http://localhost:8080/api/health"`.
 //		app.AddFunc("/api/health", func(w http.ResponseWriter, r *http.Request) {
-//			// This endpoint only responds on GET HTTP method.
+//			// This endpoint only responds to GET HTTP method.
 //			if !route.EnsureMethod(w, r, http.MethodGet) {
 //				log.Printf("source=%q method=%q error=%q\n",
 //					"/api/health",
@@ -375,15 +391,46 @@ func EnsureMethods(w http.ResponseWriter,
 //			log.Printf("source=%q msg=%q\n", "/api/health", "ok")
 //		})
 //
-//		log.Printf("source=%q msg=%q\n", "main", "up and running")
-//
 //		// A web service is reachable at
 //		// http://localhost:8080
 //		// or
 //		// http://127.0.0.1:8080 .
-//		err := http.ListenAndServe(":8080", app)
-//		if err != nil {
-//			log.Fatalf("source=%q err=%q\n", "main", err)
+//		server := &http.Server{
+//			Addr:         addr,
+//			Handler:      app,
+//			ReadTimeout:  ttl * time.Second,
+//			WriteTimeout: ttl * time.Second,
+//			BaseContext: func(net.Listener) context.Context {
+//				return ctx
+//			},
+//		}
+//
+//		// Starting serving HTTP requests.
+//		go func() {
+//			err := server.ListenAndServe()
+//			if err != nil && err != http.ErrServerClosed {
+//				log.Fatalf("source=%q error=%q\n", "server.ListenAndServe", err)
+//			}
+//		}()
+//
+//		log.Printf("source=%q msg=%q\n", "main", "up and running")
+//
+//		// Serving HTTP requests until shutting down.
+//		<-ctx.Done()
+//
+//		log.Printf("source=%q msg=%q\n", "main", "shutting down")
+//
+//		// Stopping listening to OS signals and cleaning up related resources asap.
+//		shutdown()
+//
+//		// Waiting gracefully.
+//		grace, disgrace := context.WithTimeout(context.Background(), ttl*time.Second)
+//		defer disgrace()
+//
+//		log.Printf("source=%q msg=%q\n", "main", "graceful shutdown")
+//
+//		if err := server.Shutdown(grace); err != nil {
+//			log.Printf("source=%q msg=%q reason=%q\n", "main", "forced shutdown", err)
 //		}
 //	}
 type Route struct {
